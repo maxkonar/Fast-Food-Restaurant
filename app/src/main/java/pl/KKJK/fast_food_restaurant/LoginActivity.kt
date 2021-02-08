a3ad5d035ad7bfd95706ffbc0b7e1c6da28f0433
maxkonar <kamil_karasiewicz@op.pl>
2021-02-08 21:20:46 +0100

    idk

--- app/src/main/java/pl/KKJK/fast_food_restaurant/LoginActivity.kt
+++ app/src/main/java/pl/KKJK/fast_food_restaurant/LoginActivity.kt
@@ -6,11 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-<<<<<<< HEAD
-=======
+
 import android.widget.Toast
 import androidx.annotation.RequiresApi
->>>>>>> master
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.P)
@@ -18,41 +16,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-<<<<<<< HEAD
 
-        fun nameAndPasswordChecker(){
+        fun nameAndPasswordChecker() {
             var login = findViewById<EditText>(R.id.login_input).text
             var password = findViewById<EditText>(R.id.password_input).text
-            for (customer in Customers.customers){
-                if (customer.Login == login.toString() && customer.Password == password.toString() ) {
+
+            for (customer in Customers.customers) {
+                if (customer.Login == login.toString() && customer.Password == password.toString()) {
                     val login_intent = Intent(this, MainMenu::class.java)
                     startActivity(login_intent)
                 }
             }
-=======
-
-        fun login(){
-            if(findViewById<EditText>(R.id.login_input).text.toString().isEmpty()){
-
+        }
+            findViewById<Button>(R.id.login_button).setOnClickListener() {
+                nameAndPasswordChecker()
             }
-            else{
-
-            }
-        }
-
-        findViewById<Button>(R.id.register_button).setOnClickListener() {
-            val login_intent = Intent(this, Register::class.java)
-            startActivity(login_intent)
-        }
-
-        findViewById<Button>(R.id.login_button).setOnClickListener() {
-            login()
->>>>>>> master
-        }
-
-        findViewById<Button>(R.id.login_button).setOnClickListener() {
-            nameAndPasswordChecker()
-        }
     }
 }
 
--- app/src/main/java/pl/KKJK/fast_food_restaurant/Product.kt
+++ app/src/main/java/pl/KKJK/fast_food_restaurant/Product.kt
@@ -2,17 +2,17 @@ package pl.KKJK.fast_food_restaurant
 
 object Products {
     val products: List<Product> = listOf(
-            Product(
-                    name = "testowy-produkt",
-                    description = "testowy opis",
-                    price = 10,
-                    category = "Drinks"
-            ),
-            Product(
-                    name = "testowy-produkt2",
-                    description = "testowy opis3",
-                    price = 10,
-                    category = "Burgers"
+        Product(
+            name = "testowy-produkt",
+            description = "testowy opis",
+            price = 10,
+            category = "Drinks"
+        ),
+        Product(
+            name = "testowy-produkt2",
+            description = "testowy opis3",
+            price = 10,
+            category = "Burgers"
         )
     )
 }
