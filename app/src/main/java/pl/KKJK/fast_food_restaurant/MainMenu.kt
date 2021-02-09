61481dd2a66c1868e73e70c47d7e36786487af6d
maxkonar <kamil_karasiewicz@op.pl>
2021-02-09 19:18:38 +0100

    Cleaner

--- app/src/main/java/pl/KKJK/fast_food_restaurant/MainMenu.kt
+++ app/src/main/java/pl/KKJK/fast_food_restaurant/MainMenu.kt
@@ -5,6 +5,8 @@ import android.content.ClipData
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 
@@ -15,8 +17,6 @@ class MainMenu : AppCompatActivity() {
         setContentView(R.layout.activity_main_menu)
 
         val stateValue = savedInstanceState?.getString(STATE_VALUE_1)
-
-
     }
 
     companion object {
